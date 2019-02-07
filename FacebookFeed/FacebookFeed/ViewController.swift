35301c95ab94e1e58f9cf5b425c681e5c42206d0
irna fitriani <irna.fitriani@iconpln.co.id>
2019-02-07 10:33:48 +0700

    Initial Commit

---
+++ FacebookFeed/FacebookFeed/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FacebookFeed
+//
+//  Created by irna on 07/02/19.
+//  Copyright © 2019 irna. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Facebook Feed"
+    }
+
+
+}
+
